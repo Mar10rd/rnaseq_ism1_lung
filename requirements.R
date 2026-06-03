bf74caf99322450cd901a47b8cba6e999ecfe028
Mario Antonio Rodriguez Diaz <mario99rd@gmail.com>
2026-06-04 00:00:11 +0100

    Add requirements for R packages
    Added R packages required for analysis, including Bioconductor and CRAN packages.

---
+++ requirements.R
@@ -0,0 +1,11 @@
+# R packages required to run this analysis
+# Install with: install.packages("pkg") or BiocManager::install("pkg")
+
+# Bioconductor
+# install.packages("BiocManager")
+# BiocManager::install(c("DESeq2", "Rsubread", "edgeR", "limma",
+#                        "affy", "QuasR", "pheatmap", "biomaRt", "fgsea"))
+
+# CRAN
+# install.packages(c("ggplot2", "gplots", "stringr", "dplyr",
+#                    "RColorBrewer", "patchwork", "scatterplot3d"))
